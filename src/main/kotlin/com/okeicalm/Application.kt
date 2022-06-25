6f40ad2a17f26e83e1f43654c69340144733b11c
okeicalm <okeicalm@gmail.com>
2022-06-25 16:31:23 +0900

    Initial commit

---
+++ src/main/kotlin/com/okeicalm/Application.kt
@@ -0,0 +1,11 @@
+package com.okeicalm
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.okeicalm.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
