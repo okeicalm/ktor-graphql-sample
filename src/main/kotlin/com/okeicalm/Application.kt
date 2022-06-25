5e88bed3769e0cfa1d28daf2c8d0bb59c2579fd1
okeicalm <okeicalm@gmail.com>
2022-06-25 18:00:03 +0900

    Setup application.conf

--- src/main/kotlin/com/okeicalm/Application.kt
+++ src/main/kotlin/com/okeicalm/Application.kt
@@ -1,11 +1,10 @@
 package com.okeicalm
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import com.okeicalm.plugins.*
+import com.okeicalm.plugins.configureRouting
+import io.ktor.server.application.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
 }
